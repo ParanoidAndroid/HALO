772ac632674ecb8db8a03fbf36b7a33fed7f8548
SferaDev <sferarc@gmail.com>
2013-06-11 17:36:51 +0200

    Halo Notes (Milestone 3)
    Now at the Preference Activity shows in the Summary the content of the
    note. WIP: Still you need to reload the Activity to show it.

--- src/com/paranoid/halo/ExtensionsActivity.java
+++ src/com/paranoid/halo/ExtensionsActivity.java
@@ -1,7 +1,11 @@
 package com.paranoid.halo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class ExtensionsActivity extends PreferenceActivity {
     @SuppressWarnings("deprecation")
@@ -9,5 +13,24 @@ public class ExtensionsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.extensions);
+        
+        Preference pref_note1 = findPreference("ext_note1");
+        Preference pref_note2 = findPreference("ext_note2");
+        Preference pref_note3 = findPreference("ext_note3");
+        Preference pref_note4 = findPreference("ext_note4");
+
+    	SharedPreferences ext_prefs = PreferenceManager.getDefaultSharedPreferences(ExtensionsActivity.this);
+        
+        String note1 = ext_prefs.getString("ext_note1", null);
+		String note2 = ext_prefs.getString("ext_note2", null);
+		String note3 = ext_prefs.getString("ext_note3", null);
+		String note4 = ext_prefs.getString("ext_note4", null);
+        
+		pref_note1.setSummary(note1);
+		pref_note2.setSummary(note2);
+		pref_note3.setSummary(note3);
+		pref_note4.setSummary(note4);
+        
     }
+    
 }
