71371d65fe3e1ea7c776778eed305fb17e8f99ad
Kyler <mbqsniper@gmail.com>
2013-08-14 11:11:21 -0700

    Because

--- src/com/paranoid/halo/HelperActivity.java
+++ src/com/paranoid/halo/HelperActivity.java
@@ -9,7 +9,5 @@ public class HelperActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_helper);
-		
 	}
-
 }
